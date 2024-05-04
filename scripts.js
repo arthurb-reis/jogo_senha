9f839e5d88ede11107d282c714da01fbb7cf8b36
Arthur Reis <99774782+arthurb-reis@users.noreply.github.com>
2024-05-04 17:50:45 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,8 +3,9 @@
 var senha = ['1','2','2','3'];
 var tentativa = [null, null, null, null];
 var tentativaAtual = 1; // Variável para armazenar o número da tentativa atual
+var emJogo = true;
 
-var bolas = document.querySelectorAll('.jogada1 .bola');
+var bolas;
 const teclas = document.querySelectorAll('.tecla');
 
 let corSelecionada = ''; // Variável para armazenar a cor selecionada
@@ -14,13 +15,10 @@ function atribuirBolas(tentativaAtual) {
     bolas = document.querySelectorAll('.jogada' + tentativaAtual + ' .bola');
     bolas.forEach((bola, index) => {
       bola.addEventListener('click', () => {
-        if(bolaSelecionada == index)
+        if(bolaSelecionada !== null){
             zeraSelecao();
-        else if(bolaSelecionada != null){
-            zeraSelecao();
-            seleciona(index);
-        } else
-            seleciona(index);
+        }
+        seleciona(index);
       });
     });
     seleciona(0);
@@ -30,15 +28,20 @@ function atribuirBolas(tentativaAtual) {
 document.addEventListener('DOMContentLoaded', function() {
   // Coloque aqui o nome da função que você quer chamar ao abrir a página
   atribuirBolas(1);
-  
+  embaralha();
+  emJogo = true;
 });
 
 // Adicionando observador de eventos para cada classe de bola do teclado
 teclas.forEach((tecla, index) => {
   tecla.addEventListener('click', () => {
+    if(!emJogo){
+        mostrarMensagem("mensagem-fim");
+        return;
+    }
     corSelecionada = getComputedStyle(tecla).backgroundColor; // Atualizando a cor selecionada com a cor da tecla clicada
     if(bolaSelecionada == null){
-        console.log("Selecione uma bola");
+        mostrarMensagem("mensagem-selecionar");
     } else {
         bolas[bolaSelecionada].style.backgroundColor = corSelecionada;
         tentativa[bolaSelecionada] = retornaCor(corSelecionada);
@@ -49,28 +52,35 @@ teclas.forEach((tecla, index) => {
 
 document.body.addEventListener('click', (event) => {
   // Verifica se o clique ocorreu fora das bolas e teclas
-  if (!event.target.closest('.jogada' + tentativaAtual + ' .bola') && !event.target.closest('.teclado-container') && !event.target.closest('.submit-container')) {
+  if (emJogo && !Array.from(bolas).includes(event.target) && !event.target.closest('.teclado-container') && !event.target.closest('.submit-container')) {
     zeraSelecao();
   }
 });
 
 function zeraSelecao(){
-    if(bolaSelecionada != null){
+    if(bolaSelecionada !== null && emJogo){
         var corRGBA = getComputedStyle(bolas[bolaSelecionada]).backgroundColor;
-        var corRGB = corRGBA.replace(/,\s*\d+(\.\d+)?\s*\)/, ')');
-        bolas[bolaSelecionada].style.backgroundColor = corRGB;
+        if (corRGBA.includes('rgba')) {
+            var corRGB = corRGBA.replace(/,\s*\d+(\.\d+)?\s*\)/, ')').replace('rgba', 'rgb');
+            bolas[bolaSelecionada].style.backgroundColor = corRGB;
+        } else {
+            bolas[bolaSelecionada].style.backgroundColor = corRGBA;
+        }
         bolaSelecionada = null;
     }
 }
 
 function seleciona(index){
+    if(!emJogo)
+        return;
     if(index < 4){
         var corRGB = getComputedStyle(bolas[index]).backgroundColor;
-        var indiceUltimoNumero = corRGB.lastIndexOf(')'); // Encontra o índice da última vírgula
-        var corRGBA = corRGB.slice(0, indiceUltimoNumero) + ', 0.6' + corRGB.slice(indiceUltimoNumero);
-
-        // Torna a bola clicada um pouco mais clara
-        bolas[index].style.backgroundColor = corRGBA;
+        if(!corRGB.includes('rgba')){
+            var indiceUltimoNumero = corRGB.lastIndexOf(')'); // Encontra o índice da última vírgula
+            var corRGBA = corRGB.slice(0, indiceUltimoNumero) + ', 0.6' + corRGB.slice(indiceUltimoNumero);
+            // Torna a bola clicada um pouco mais clara
+            bolas[index].style.backgroundColor = corRGBA.replace('rgb', 'rgba');
+        }
         bolaSelecionada = index; // Armazena o índice da bola selecionada
     } else {
         bolaSelecionada = null;
@@ -78,15 +88,15 @@ function seleciona(index){
 }
 
 function retornaCor(RGB){
-    if(RGB === 'rgb(255, 0, 0)'){
+    if(RGB === 'rgb(0, 9, 56)'){
         return '0';
-    } else if(RGB === 'rgb(0, 128, 0)'){
+    } else if(RGB === 'rgb(163, 119, 0)'){
         return '1';
-    } else if(RGB === 'rgb(0, 0, 255)'){
+    } else if(RGB === 'rgb(0, 71, 5)'){
         return '2';
-    } else if(RGB === 'rgb(255, 255, 0)'){
+    } else if(RGB === 'rgb(178, 47, 14)'){
         return '3';
-    } else if(RGB === 'rgb(255, 165, 0)'){
+    } else if(RGB === 'rgb(71, 11, 21)'){
         return '4';
     }
     return null;
@@ -95,10 +105,13 @@ function retornaCor(RGB){
 document.getElementById("try").addEventListener("click", verificar);
 
 function verificar() {
+    if(!emJogo){
+        mostrarMensagem("mensagem-fim");
+        return;
+    }
     for (let i = 0; i < tentativa.length; i++) {
         if (tentativa[i] === null) {
-            console.log("Selecione uma bola");
-
+            mostrarMensagem("mensagem-try");
             return;
         }
     }
@@ -131,13 +144,20 @@ function verificar() {
 
     pintaResposta(vermelhos, brancos);
 
-    if(vermelhos >= 4)
-        console.log("Ganhou");
+    if(vermelhos >= 4){
+        mostrarMensagemFim('#mensagemModalWin');
+        emJogo = false;
+    }
 
     tentativaAtual++;
-    if(tentativaAtual >= 11)
-        console.log("Perdeu");
+    if(tentativaAtual >= 11){
+        mostrarMensagemFim('#mensagemModal');
+        emJogo = false;
+        pintaSenhaCorreta();
+        mostrarMensagemCorreta();
+    }
     zeraSelecao();
+    bolas = null;
     atribuirBolas(tentativaAtual);
 }
 
@@ -146,9 +166,71 @@ function pintaResposta(V, B){
     const bolasr = document.querySelectorAll('.jogada' + tentativaAtual + ' .bolar');
     var i = 0;
     for(; i < V; i++)
-        bolasr[i].style.backgroundColor = 'red';
+        bolasr[i].style.backgroundColor = '#cf0c0c';
     for(; i < V+B; i++)
-        bolasr[i].style.backgroundColor = 'white';
+        bolasr[i].style.backgroundColor = '#F5F5F5';
     for(; i < 4; i++)
         bolasr[i].style.backgroundColor = 'black';
+}
+
+function embaralha(){
+    senha = [];
+    Math.random();
+    // Preenche o vetor senha com elementos aleatórios
+    for (var i = 0; i < 4; i++) {
+        var valorAleatorio;
+        
+        // Garante que o valor aleatório não seja igual aos últimos dois elementos
+        do {
+            valorAleatorio = Math.floor(Math.random() * 5).toString(); // Gera um valor aleatório entre 0 e 3
+        } while (i >= 2 && valorAleatorio === senha[i - 1] && valorAleatorio === senha[i - 2]);
+        
+        senha.push(valorAleatorio);
+    }
+}
+
+
+function mostrarMensagem(mensagem) {
+    var divMensagem = document.getElementById(mensagem);
+    divMensagem.style.display = "block"; // Garante que o elemento esteja visível
+
+    // Após um pequeno intervalo, alteramos a opacidade para 0, dando a sensação de desaparecimento suave
+    setTimeout(function() {
+        divMensagem.style.opacity = "0"; // Diminui a opacidade
+        // Após a transição, definimos a propriedade de exibição para "none" para esconder completamente o elemento
+        setTimeout(function() {
+            divMensagem.style.display = "none";
+        }, 1000); // Tempo para a transição terminar
+    }, 1500); // Tempo para a mensagem ser exibida
+    divMensagem.style.opacity = "1";
+}
+
+function mostrarMensagemCorreta() {
+    var divMensagem = document.getElementById('mensagem-correta');
+    divMensagem.style.display = "block"; // Garante que o elemento esteja visível
+    divMensagem.style.opacity = "1"; // Define a opacidade para 1, tornando o elemento completamente visível
+}
+
+function mostrarMensagemFim(mensagem) {
+    $(mensagem).modal({ backdrop: 'static', keyboard: false }); // Mostra o modal sem permitir fechar com clique fora ou com o teclado
+}
+
+
+function pintaSenhaCorreta() {
+    for(var i = 0; i < 4; i++){
+        var RGB = null;
+        var bolaC = document.querySelectorAll('.bolac' + i);
+        if(senha[i] === '0'){
+            RGB = 'rgb(0, 9, 56)';
+        } if(senha[i] === '1'){
+            RGB = 'rgb(163, 119, 0)';
+        } else if(senha[i] === '2'){
+            RGB = 'rgb(0, 71, 5)'
+        } else if(senha[i] === '3'){
+            RGB = 'rgb(178, 47, 14)';
+        } else if(senha[i] === '4'){
+            RGB = 'rgb(71, 11, 21)';
+        }
+        bolaC[0].style.backgroundColor = RGB;
+    }
 }
